867b67f95fa6dc35d61ccda529f858caaa13a080
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2025-03-31 18:05:58 +0500

    Add project files.

---
+++ ClassroomConnect/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClassroomConnect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

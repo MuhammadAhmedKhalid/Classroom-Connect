a7401d1e9f6fb19009df639b9a26af61f7cf3bd5
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2025-04-02 21:27:03 +0500

    Dependencies versions updated + Identity added

--- Classroom.DataAccess/Data/ApplicationDbContext.cs
+++ Classroom.DataAccess/Data/ApplicationDbContext.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Classroom.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Classroom.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<ClassroomDetail> ClassroomDetails { get; set; }
+
     }
 }
--- ClassroomConnect/Program.cs
+++ ClassroomConnect/Program.cs
@@ -1,5 +1,6 @@
 using Classroom.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,12 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+
+builder.Services.AddDefaultIdentity<IdentityUser>(
+    //options => options.SignIn.RequireConfirmedAccount = true
+    ).AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -21,7 +28,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
